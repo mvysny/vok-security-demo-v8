877f29f9374f8657e8e5abbd1c26db6e02b96523
Martin Vysny <martin@vysny.me>
2018-10-19 11:48:32 +0300

    gradle kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.*
+
+plugins {
+    kotlin("jvm") version "1.2.71"
+    // need to use Gretty here because of https://github.com/johndevs/gradle-vaadin-plugin/issues/317
+    id("org.gretty") version "2.2.0"
+    id("com.devsoap.plugin.vaadin") version "1.4.1"
+}
+
+defaultTasks("clean", "build")
+
+allprojects {
+    group = "com.example.vok"
+    version = "1.0-SNAPSHOT"
+
+    // Heroku
+    repositories {
+        jcenter()
+        maven { setUrl("https://dl.bintray.com/mvysny/github") }
+    }
+
+    tasks {
+        // Heroku
+        val stage by registering {
+            dependsOn("build")
+        }
+    }
+}
+
+subprojects {
+
+    apply { plugin("kotlin") }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            // to see the exceptions of failed tests in Travis-CI console.
+            exceptionFormat = TestExceptionFormat.FULL
+        }
+    }
+}
