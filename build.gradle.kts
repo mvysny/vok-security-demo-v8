4e225a083545585f85f9efe335b8f861b2c52e32
Martin Vysny <mavi@vaadin.com>
2019-12-09 11:19:51 +0200

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.util.*
 
 plugins {
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.3.61"
     // need to use Gretty here because of https://github.com/johndevs/gradle-vaadin-plugin/issues/317
     id("org.gretty") version "2.3.1"
     id("com.devsoap.plugin.vaadin") version "1.4.1"
--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -21,9 +21,9 @@ dependencies {
     // logging
     // currently we are logging through the SLF4J API to LogBack. See logback.xml file for the logger configuration
     compile("ch.qos.logback:logback-classic:1.2.3")
-    compile("org.slf4j:slf4j-api:1.7.25")
+    compile("org.slf4j:slf4j-api:1.7.28")
     // this will configure Vaadin to log to SLF4J
-    compile("org.slf4j:jul-to-slf4j:1.7.25")
+    compile("org.slf4j:jul-to-slf4j:1.7.28")
 
     // workaround until https://youtrack.jetbrains.com/issue/IDEA-178071 is fixed
     compile("com.vaadin:vaadin-themes:${properties["vaadin8_version"]}")
@@ -32,18 +32,18 @@ dependencies {
     providedCompile("javax.servlet:javax.servlet-api:3.1.0")
 
     // db
-    compile("org.flywaydb:flyway-core:5.2.4")
-    compile("com.h2database:h2:1.4.198")
+    compile("org.flywaydb:flyway-core:6.1.0")
+    compile("com.h2database:h2:1.4.200")
 
     // Kotlin
     compile(kotlin("stdlib-jdk8"))
 
     // test
     testCompile("com.github.mvysny.dynatest:dynatest-engine:0.15")
-    testCompile("com.github.mvysny.kaributesting:karibu-testing-v8:1.1.9")
+    testCompile("com.github.mvysny.kaributesting:karibu-testing-v8:1.1.17")
 
     // heroku app runner
-    staging("com.github.jsimone:webapp-runner:9.0.24.0")
+    staging("com.github.jsimone:webapp-runner:9.0.27.1")
 }
 
 // Heroku
